ff8a8390f8c0d0791eb16f6b49b4b79f59292b73
Praveen CH <praveen4832@gmail.com>
2024-01-14 12:32:19 +0530

    first project

---
+++ 01_selector.py
@@ -0,0 +1,11 @@
+"""
+simply a random selector to tell me who to facetime! (deals with imports) .
+"""
+
+names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hank", "Ivy", "Jack"]
+
+def facetime(names):
+    return names.random
+
+rando = facetime()
+print rando
